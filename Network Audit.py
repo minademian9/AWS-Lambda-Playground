f2f1563e206b6f18898a6b23e2e67a8d430a5e06
Mina Demian <minademian9@gmail.com>
2021-02-15 10:59:35 +0200

    Added All Protocols tests

--- Network Audit.py
+++ Network Audit.py
@@ -18,11 +18,23 @@ def lambda_handler(event, context):
     sg_all = ec2.describe_security_groups()
     for sg in sg_all['SecurityGroups'] :
         # print(sg['GroupName'])
+        # print(sg)
         for rule in sg['IpPermissions']:
+            
+            # Checking if all protocols are allowed
+            
+            if rule['IpProtocol'] == '-1':
+                failed_sg.append({'Name': sg['GroupName'], 'Id': sg['GroupId']  , "Reason": "All Protocols Allowed"  })
+                
+                
+            # Checking if All IPs are allowed on specfic ports
+            
             for ip in rule['IpRanges']:
                 if ip['CidrIp'] == "0.0.0.0/0":
                     if (rule['FromPort'] not in [80,443]) and (rule['ToPort'] not in [80,443]):
                         failed_sg.append({'Name': sg['GroupName'], 'Id': sg['GroupId']  , "Reason": "All IPs allowed from port " + str(rule['FromPort']) + " to port " + str(rule['ToPort'])  })
+                
+            
 
 
     print(failed_sg)
