2e97ea154e0caf132a1df6238c650ba0f3cebfea
Mina Demian <minademian9@gmail.com>
2021-02-16 13:13:42 +0200

    Added if statement for checking ports & protocols

--- NACL Network Audit.js
+++ NACL Network Audit.js
@@ -7,7 +7,16 @@ ec2.describeNetworkAcls(params = {}, ( function(err, data) {
    else     console.log(data);           // successful response
  });
 
+// for nacl in nacl_df.loc[index,'Entries']:
+//         if nacl['RuleAction'] == 'allow' and nacl["CidrBlock"] == "0.0.0.0/0":
+//             if 'PortRange' in nacl:
+//                         if not ((nacl['PortRange']['From']==80 and nacl['PortRange']['To']==80) or (nacl['PortRange']['From']==443 and nacl['PortRange']['To']==443)or (nacl['PortRange']['From']>=1024 and nacl['PortRange']['To']>=65535) or (nacl['PortRange']['From']>=32768 and nacl['PortRange']['To']>=32768)):
 
+
+// for index,val in nacl_df.iterrows():
+//    for nacl in nacl_df.loc[index,'Entries']:
+//       if nacl['RuleAction'] == 'allow'and nacl['Protocol'] == '-1':
+//          failflag = True
 
 // exports.handler = async (event) => {
 //     // TODO implement
