f3560a989cbc113f6d59ed2040f0c9a1540d6240
Mina Demian <minademian9@gmail.com>
2021-02-16 15:04:38 +0200

    Fixed handler
    removed async to let the describe function run first

--- NACL Network Audit.js
+++ NACL Network Audit.js
@@ -2,7 +2,7 @@ const aws = require('aws-sdk')
 
 const ec2 = new aws.EC2()
 
-ec2.describeNetworkAcls(params = {}, ( function(err, data) {
+ec2.describeNetworkAcls(params = {}, function(err, data) {
    if (err) console.log(err, err.stack); // an error occurred
    else     console.log(data);           // successful response
  });
@@ -18,12 +18,12 @@ ec2.describeNetworkAcls(params = {}, ( function(err, data) {
 //       if nacl['RuleAction'] == 'allow'and nacl['Protocol'] == '-1':
 //          failflag = True
 
-// exports.handler = async (event) => {
-//     // TODO implement
-//     const response = {
-//         statusCode: 200,
-//         body: JSON.stringify('Hello from Lambda!')
-//         // body : ec2.describeNetworkAcls()
-//     };
-//     return response;
-// };
+exports.handler =  (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!')
+        // body : ec2.describeNetworkAcls()
+    };
+    return response;
+};
